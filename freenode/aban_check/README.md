6e5bf6576d1187d945f5ee22e89096596f921b8b
jesopo <github@lolnerd.net>
2020-06-25 16:44:49 +0100

    aban_check: walk down $j: bans, dump yaml instead of json

--- freenode/aban_check/README.md
+++ freenode/aban_check/README.md
@@ -2,18 +2,18 @@
 
 ## requirements
 
-> $ pip3 install ircrobots
+> $ pip3 install ircrobots pyyaml
 
 ## usage
-> $ ./aban_check.py mybotnick "#mychan" out.json
+> $ ./aban_check.py mybotnick "#mychan" out.yaml
 
 ## behaviour
 
 this bot will connect to freenode, query the banlist for the given channel,
 pick out the account (`$a:`) bans and then query NickServ for each account to
-see which are still registered and which are not. the outfile will be a json
-dictionary of `"nickname": bool` where the bool represents if the account is
-still registered.
+see which are still registered and which are not. the outfile will be a yaml
+dictionary of nicknames, whether they are registered or not, and what channel
+the ban comes from (for `$j:` ban trees.)
 
 ## beware
 
--- freenode/aban_check/aban_check.py
+++ freenode/aban_check/aban_check.py
@@ -1,7 +1,9 @@
-import asyncio, json, sys
+import asyncio, sys
 
-from typing   import List
+from typing   import Any, Dict, List, Optional, Tuple
 from argparse import ArgumentParser
+
+import yaml
 
 from irctokens import build, Line
 from ircrobots import Bot as BaseBot
@@ -25,28 +27,63 @@ RESP_END   = Response("NOTICE", [SELF, Formatless("*** End of Info ***")],
     source=NICKSERV)
 
 class Server(BaseServer):
+    async def _ban_list(self,
+            chan: str,
+            depth=0
+            ) -> Optional[List[Tuple[(List[str], str, int)]]]:
+
+        await self.send(build("MODE", [chan, "+b"]))
+
+        masks: List[Tuple[List[str], str, int]] = []
+        while True:
+            line = await self.wait_for(Responses(
+                [RPL_BANLIST, RPL_ENDOFBANLIST, ERR_NOSUCHCHANNEL],
+                [SELF, Folded(chan)]
+            ))
+
+            if line.command == ERR_NOSUCHCHANNEL:
+                return None
+            elif line.command == RPL_ENDOFBANLIST:
+                break
+            else:
+                mask   = line.params[2]
+                set_by = line.params[3]
+                set_at = int(line.params[4])
+                masks.append(([mask], set_by, set_at))
+
+        if depth == 0:
+            for (mask,), _, _ in list(masks):
+                if mask.startswith("$j:"):
+                    nextchan = mask.split(":", 1)[1].split("$", 1)[0]
+                    nextchan_masks = await self._ban_list(nextchan, depth + 1)
+                    if nextchan_masks is not None:
+                        for nextmask, set_by, set_at in nextchan_masks:
+                            masks.append((nextmask+[mask], set_by, set_at))
+
+        return masks
+
     async def line_read(self, line: Line):
         if line.command == "001":
             await self.send(build("MODE", [CHAN, "+b"]))
 
-            masks: List[str] = []
-            while True:
-                line = await self.wait_for(Responses(
-                    [RPL_BANLIST, RPL_ENDOFBANLIST],
-                    [ANY, Folded(CHAN), ANY]
-                ))
-                if line.command == RPL_BANLIST:
-                    masks.append(line.params[2])
-                else:
-                    break
+            chan_bans = await self._ban_list(CHAN)
+            if chan_bans is None:
+                sys.stderr.write(f"{CHAN} not found\n")
+                sys.exit(1)
 
-            accounts: List[str] = []
-            for mask in masks:
+            accounts: Dict[str, List[str]] = {}
+            for mask_tree, set_by, set_at in chan_bans:
+                mask = mask_tree[0]
                 if mask.startswith("$a:"):
-                    accounts.append(mask.split(":", 1)[1])
+                    account = mask.split(":", 1)[1].split("$", 1)[0]
+                    account = self.casefold(account)
+                    if not account in accounts:
+                        accounts[account] = []
+
+                    accounts[account].append((mask_tree[1:] or [CHAN])[0])
 
             states: Dict[str, bool] = {}
-            for account in accounts:
+            for account in accounts.keys():
                 await self.send(build("NS", ["INFO", account]))
                 line = await self.wait_for({
                     RESP_REG, RESP_UNREG
@@ -57,8 +94,19 @@ class Server(BaseServer):
                     await self.wait_for(RESP_END)
                 else:
                     states[account] = False
+
+            states_where: Dict[str, Any] = {}
+            for account in sorted(accounts.keys()):
+                state = {
+                    "registered": states[account],
+                    "sources":    accounts[account]
+                }
+                states_where[account] = state
+
             with open(FILE, "w") as outfile:
-                outfile.write(json.dumps(states, indent=4, sort_keys=True))
+                outfile.write(
+                    yaml.dump(states_where)
+                )
                 outfile.write("\n")
             print(f"! written to {FILE}")
             sys.exit()
