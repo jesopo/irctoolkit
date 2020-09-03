c81d9a6b404ca5e082d9bcca8322d5663ad72bd5
jesopo <github@lolnerd.net>
2020-09-03 23:51:44 +0000

    add autoaway2.py

---
+++ weechat/autoaway2.py
@@ -0,0 +1,47 @@
+try:
+    import weechat as w
+    import_ok = True
+except ImportError:
+    print("This script must be run under WeeChat")
+    print("Get WeeChat now at: https://weechat.org/")
+    import_ok = False
+
+SCRIPT_NAME    = "autoaway2"
+SCRIPT_AUTHOR  = "jesopo"
+SCRIPT_VERSION = "0.1"
+SCRIPT_LICENSE = "MIT"
+SCRIPT_DESC    = "auto_away but cooler"
+
+def idle_check(data, remain):
+    try:
+        timeout = int(w.config_get_plugin("timeout"))
+    except ValueError:
+        timeout = 0
+    message    = w.config_get_plugin("message")
+    inactivity = int(w.info_get("inactivity", "0")) / 60
+
+    if timeout > 0 and inactivity >= timeout:
+        servers = w.infolist_get("irc_server", "", "")
+        while w.infolist_next(servers):
+            if (w.infolist_integer(servers, "is_connected") == 1 and
+                    w.infolist_integer(servers, "is_away") == 0):
+                ptr = w.infolist_pointer(servers, "buffer")
+                w.command(ptr, f"/away {message}")
+        w.infolist_free(servers)
+
+    return w.WEECHAT_RC_OK
+
+SETTINGS = {
+    "timeout": ["20",   "Minutes of inactivity before autoaway"],
+    "message": ["Idle", "Autoaway message"]
+}
+
+if import_ok and w.register(SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION, SCRIPT_LICENSE, SCRIPT_DESC, "", ""):
+    for name, (default, description) in SETTINGS.items():
+        if not w.config_is_set_plugin(name):
+            w.config_set_plugin(name, default)
+            w.config_set_desc_plugin(name, description)
+
+    # every 60 seconds, on 00
+    w.hook_timer(60 * 1000, 60, 0, "idle_check", "")
+
