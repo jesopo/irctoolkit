ca2cf5e885d8df28c777a6d62c7288f22b6f3f09
jesopo <github@lolnerd.net>
2020-09-19 21:33:31 +0000

    (betteritems.py) guess we need to provide everything input_buffer does

--- weechat/betteritems.py
+++ weechat/betteritems.py
@@ -23,10 +23,6 @@ DEFAULT_CMODE = {
     "m": "214"
 }
 
-def items_modes_update():
-    w.bar_item_update("better_umodes")
-    w.bar_item_update("better_cmodes")
-
 def default_umodes(server):
     modes = {}
     for mode, color in DEFAULT_UMODES.items():
@@ -62,19 +58,22 @@ def bar_item_umodes(data, item, window):
     server = w.buffer_get_string(buffer, "localvar_server")
 
     if server:
-        uinfo = w.infolist_get("irc_Server", '', server)
+        uinfo = w.infolist_get("irc_server", '', server)
         w.infolist_next(uinfo)
         modes = w.infolist_string(uinfo, "nick_modes")
         w.infolist_free(uinfo)
 
         modes  = list(sorted(modes))
-        reset  = w.color("reset")
         modesf = mode_setting("umodes", server) or default_umodes(server)
 
         for i, mode in enumerate(modes):
             if mode in modesf:
                 color = w.color(modesf[mode])
-                modes[i] = f"{color}{mode}{reset}"
+            else:
+                color = w.color(w.config_string(w.config_get(
+                    "irc.color.item_nick_modes"
+                )))
+            modes[i] = f"{color}{mode}"
 
         return "".join(modes)
     else:
@@ -109,16 +108,18 @@ def bar_item_cmodes(data, item, window):
         args.clear()
         modes.sort(key=lambda m: m[0])
 
-        reset  = w.color("reset")
         modesf = mode_setting("cmodes", server) or default_cmodes(server)
         for i, (mode, arg) in enumerate(modes):
             if mode in modesf:
                 color = w.color(modesf[mode])
-                mode = f"{color}{mode}{reset}"
-                if arg:
-                    arg  = f"{color}{arg}{reset}"
+            else:
+                color = w.color(w.config_string(w.config_get(
+                    "irc.color.item_channel_modes"
+                )))
+
+            mode = f"{color}{mode}"
             if arg:
-                args.append(arg)
+                args.append(f"{color}{arg}")
             modes[i] = mode
 
         args = " ".join(args)
@@ -127,18 +128,100 @@ def bar_item_cmodes(data, item, window):
         return "+" + "".join(modes) + args
     return ""
 
-def config_all(data, buffer, args):
-    items_modes_update()
+def bar_item_prefix(data, item, window):
+    buffer  = w.window_get_pointer(window, "buffer")
+    server  = w.buffer_get_string(buffer, "localvar_server")
+    channel = w.buffer_get_string(buffer, "localvar_channel")
+    if w.info_get("irc_is_channel", channel):
+        nick   = w.info_get("irc_nick", server)
+        ninfo  = w.infolist_get("nicklist", buffer, f"nick_{nick}")
+        w.infolist_next(ninfo)
+        prefix = w.infolist_string(ninfo, "prefix")
+        pcolor = w.infolist_string(ninfo, "prefix_color")
+        w.infolist_free(ninfo)
+
+        pcolor = w.color(pcolor)
+        return f"{pcolor}{prefix}"
+    return ""
+
+def bar_item_nick(data, item, window):
+    buffer  = w.window_get_pointer(window, "buffer")
+    server  = w.buffer_get_string(buffer, "localvar_server")
+    if server:
+        nick  = w.info_get("irc_nick", server)
+        color = w.config_string(w.config_get("irc.color.input_nick"))
+
+        color = w.color(color)
+        return f"{color}{nick}"
+    return ""
+
+def bar_item_prompt(data, item, window):
+    buffer  = w.window_get_pointer(window, "buffer")
+    server  = w.buffer_get_string(buffer, "localvar_server")
+
+    prompt  = ""
+    prompt += bar_item_prefix(data, item, window)
+    prompt += bar_item_nick(data, item, window)
+
+    umodes  = bar_item_umodes(data, item, window)
+    if umodes:
+        delim   = w.config_string(w.config_get("weechat.bar.input.color_delim"))
+        delim   = w.color(delim)
+        reset   = w.color("reset")
+        prompt += f"{delim}({reset}"
+        prompt += umodes
+        prompt += f"{delim})"
+    return prompt
+
+def items_update():
+    w.bar_item_update("better_nick")
+    w.bar_item_update("better_umodes")
+    w.bar_item_update("better_cmodes")
+    w.bar_item_update("better_prefix")
+    w.bar_item_update("better_prompt")
+
+def config_plugin(data, buffer, args):
+    items_update()
+    return w.WEECHAT_RC_OK
+def config_channel(data, buffer, args):
+    w.bar_item_update("better_cmodes")
+    w.bar_item_update("better_prompt")
+    return w.WEECHAT_RC_OK
+def config_user(data, buffer, args):
+    w.bar_item_update("better_umodes")
+    w.bar_item_update("better_prompt")
+    return w.WEECHAT_RC_OK
+def config_prefix(data, buffer, args):
+    w.bar_item_update("better_prefix")
+    w.bar_item_update("better_prompt")
+    return w.WEECHAT_RC_OK
+def config_nick(data, buffer, args):
+    w.bar_item_update("better_nick")
+    w.bar_item_update("better_prompt")
+    return w.WEECHAT_RC_OK
+def config_delim(data, buffer, args):
+    w.bar_item_update("better_prompt")
+    return w.WEECHAT_RC_OK
+
 def signal_mode(data, signal, signal_data):
-    items_modes_update()
+    items_update()
     return w.WEECHAT_RC_OK
 
 if import_ok and w.register(SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION, SCRIPT_LICENSE, SCRIPT_DESC, "", ""):
+    w.bar_item_new("better_nick",   "bar_item_nick",   "")
     w.bar_item_new("better_umodes", "bar_item_umodes", "")
     w.bar_item_new("better_cmodes", "bar_item_cmodes", "")
-    items_modes_update()
+    w.bar_item_new("better_prefix", "bar_item_prefix", "")
+    w.bar_item_new("better_prompt", "bar_item_prompt", "")
+    items_update()
 
-    w.hook_config(f"plugins.var.python.{SCRIPT_NAME}.*", "config_all", '')
+    w.hook_config(f"plugins.var.python.{SCRIPT_NAME}.*", "config_plugin",  '')
+    w.hook_config("irc.color.item_channel_modes",        "config_channel", '')
+    w.hook_config("irc.color.item_nick_modes",           "config_user",    '')
+    w.hook_config("irc.color.nick_prefixes",             "config_prefix",  '')
+    w.hook_config("irc.color.input_nick",                "config_nick",    '')
+    w.hook_config("weechat.bar.input.color_delim",       "config_delim",   '')
+
     w.hook_signal("*,irc_in_MODE", "signal_mode", '')
-    w.hook_signal("*,irc_in_221", "signal_mode", '') # RPL_UMODEIS
-    w.hook_signal("*,irc_in_324", "signal_mode", '') # RPL_CHANNELMODEIS
+    w.hook_signal("*,irc_in_221", "signal_mode",  '') # RPL_UMODEIS
+    w.hook_signal("*,irc_in_324", "signal_mode",  '') # RPL_CHANNELMODEIS
